fc68fb58776708e1d5b485bfaeec8b79a1263f8a
Aleksandr Gurinov <alexander.gurinov@gmail.com>
2022-09-30 18:09:46 +0300

    [bitset] Generic impl

---
+++ bitset/bitset_test.go
@@ -0,0 +1,67 @@
+//go:build test_unit
+
+package pl_bitset_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	pl_bitset "github.com/agurinov/gopl.git/bitset"
+)
+
+const (
+	bit1 = 1 << iota
+	bit2
+)
+
+func TestBitSet_Set(t *testing.T) {
+	t.Parallel()
+
+	bitset := pl_bitset.BitSet[uint8]{}
+
+	require.False(t, bitset.Has(bit1))
+	require.False(t, bitset.Has(bit2))
+
+	bitset.Set(bit2)
+
+	require.False(t, bitset.Has(bit1))
+	require.True(t, bitset.Has(bit2))
+}
+
+func TestBitSet_Clear(t *testing.T) {
+	t.Parallel()
+
+	bitset := pl_bitset.BitSet[uint32]{}
+
+	require.False(t, bitset.Has(bit1))
+	require.False(t, bitset.Has(bit2))
+
+	bitset.Set(bit1)
+	bitset.Set(bit2)
+	bitset.Clear(bit2)
+
+	require.True(t, bitset.Has(bit1))
+	require.False(t, bitset.Has(bit2))
+}
+
+func TestBitSet_Toggle(t *testing.T) {
+	t.Parallel()
+
+	bitset := pl_bitset.BitSet[uint]{}
+
+	require.False(t, bitset.Has(bit1))
+	require.False(t, bitset.Has(bit2))
+
+	bitset.Toggle(bit1)
+	bitset.Toggle(bit2)
+
+	require.True(t, bitset.Has(bit1))
+	require.True(t, bitset.Has(bit2))
+
+	bitset.Toggle(bit1)
+	bitset.Toggle(bit2)
+
+	require.False(t, bitset.Has(bit1))
+	require.False(t, bitset.Has(bit2))
+}
