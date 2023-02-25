dcb617953c5c9a120dfca8f4ec1893f81fb2c0c7
Aleksandr Gurinov <alexander.gurinov@gmail.com>
2023-02-25 20:58:16 +0200

    [codegen] mockgen codegen

--- codegen.go
+++ codegen.go
@@ -3,4 +3,5 @@
 package main
 
 // Define generate aliases (long shared commands)
-//go:generate -command stringer go run vendor/golang.org/x/tools/cmd/stringer/stringer.go
+//go:generate -command stringer go run ./vendor/golang.org/x/tools/cmd/stringer
+//go:generate -command mockgen go run ./vendor/github.com/golang/mock/mockgen
