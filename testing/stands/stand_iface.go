2b09875eafad2ab5d996421f99899cc3d2443841
Aleksandr Gurinov <alexander.gurinov@gmail.com>
2023-11-11 14:21:26 +0200

    fix(testing): Deal with new Makfile and integration tests

--- testing/stands/stand_iface.go
+++ testing/stands/stand_iface.go
@@ -1,6 +1,10 @@
 package stands
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 type (
 	State struct {
@@ -11,3 +15,27 @@ type (
 		Name() string
 	}
 )
+
+func Init(
+	t *testing.T,
+	si ...Interface,
+) map[string]State {
+	t.Helper()
+
+	states := make(map[string]State, len(si))
+
+	for _, stand := range si {
+		require.NotNil(t, stand)
+
+		var (
+			standName    = stand.Name()
+			standCreated = stand.Up(t)
+		)
+
+		states[standName] = State{
+			Created: standCreated,
+		}
+	}
+
+	return states
+}
--- testing/test_case.go
+++ testing/test_case.go
@@ -32,8 +32,11 @@ func (tc TestCase) Init(t *testing.T, si ...stands.Interface) map[string]stands.
 	t.Helper()
 
 	var (
-		isDebug         = tc.Debug || tc.root
-		isIntegration   = len(si) > 0
+		isDebug       = tc.Debug
+		isIntegration = len(si) > 0
+	)
+
+	var (
 		needDebug       = envvars.GDebug.Present()
 		needIntegration = !testing.Short()
 		needParallel    = !needDebug
@@ -44,34 +47,18 @@ func (tc TestCase) Init(t *testing.T, si ...stands.Interface) map[string]stands.
 		t.Skip("tc skipped: explicit skip flag")
 	case tc.Fail:
 		t.Fail()
+	case !needIntegration && isIntegration:
+		t.Skip("tc skipped: integration test during unit mode")
+	case tc.root:
 	case needDebug && !isDebug:
 		t.Skip("tc skipped: not debuggable during " + envvars.GDebug.String())
 	case needIntegration && !isIntegration:
 		t.Skip("tc skipped: unit test during integration mode")
-	case !needIntegration && isIntegration:
-		t.Skip("tc skipped: integration test during unit mode")
 	}
 
 	if needParallel {
 		t.Parallel()
 	}
 
-	states := make(map[string]stands.State, len(si))
-
-	for _, stand := range si {
-		if stand == nil {
-			continue
-		}
-
-		var (
-			standName    = stand.Name()
-			standCreated = stand.Up(t)
-		)
-
-		states[standName] = stands.State{
-			Created: standCreated,
-		}
-	}
-
-	return states
+	return stands.Init(t, si...)
 }
