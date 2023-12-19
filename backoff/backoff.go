bc2c3ca1df968dcccd97d004d5bc898e11362aa4
Aleksandr Gurinov <alexander.gurinov@gmail.com>
2023-12-19 12:16:54 +0200

    fix(backoff): Refactor backoff package

--- backoff/backoff.go
+++ backoff/backoff.go
@@ -8,19 +8,34 @@ import (
 
 	"github.com/go-playground/validator/v10"
 
+	"github.com/agurinov/gopl/backoff/strategies"
 	c "github.com/agurinov/gopl/patterns/creational"
 	pl_strings "github.com/agurinov/gopl/strings"
 )
 
 type (
 	Backoff struct {
-		strategy   Strategy
+		strategy   strategies.Interface
 		name       string
 		retries    uint32
 		maxRetries uint32
 	}
 	Option = c.Option[Backoff]
 )
+
+func New(opts ...Option) (*Backoff, error) {
+	defaultObj := Backoff{
+		name:       pl_strings.UnspecifiedPlaceholder,
+		maxRetries: math.MaxUint32,
+	}
+
+	obj, err := c.ConstructWithValidate(defaultObj, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	return &obj, nil
+}
 
 func (b *Backoff) Wait(ctx context.Context) (Stat, error) {
 	// Register new retry
@@ -54,8 +69,10 @@ func (b *Backoff) Reset() {
 
 func (b Backoff) Validate() error {
 	s := struct {
-		MaxRetries uint32 `validate:"min=1"`
+		Strategy   strategies.Interface `validate:"required"`
+		MaxRetries uint32               `validate:"min=1"`
 	}{
+		Strategy:   b.strategy,
 		MaxRetries: b.maxRetries,
 	}
 
@@ -64,24 +81,4 @@ func (b Backoff) Validate() error {
 	}
 
 	return nil
-}
-
-func New(opts ...Option) (*Backoff, error) {
-	exponentialStrategy, err := NewExponentialStrategy()
-	if err != nil {
-		return nil, err
-	}
-
-	obj := Backoff{
-		name:       pl_strings.UnspecifiedPlaceholder,
-		maxRetries: math.MaxUint32,
-		strategy:   exponentialStrategy,
-	}
-
-	obj, err = c.Construct(obj, opts...)
-	if err != nil {
-		return nil, err
-	}
-
-	return &obj, nil
 }
--- backoff/strategies/exponential_f_opt.go
+++ backoff/strategies/exponential_f_opt.go
@@ -1,4 +1,4 @@
-package backoff
+package strategies
 
 import (
 	"time"
