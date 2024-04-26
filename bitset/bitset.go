c7495098ea001d724df89b9b07e5f0164003b05b
Aleksandr Gurinov <alexander.gurinov@gmail.com>
2024-04-26 12:05:26 +0300

    fix: bitset from his type

--- bitset/bitset.go
+++ bitset/bitset.go
@@ -8,6 +8,12 @@ type BitSet[B constraints.Unsigned] struct {
 	set B
 }
 
+func New[B constraints.Unsigned](set B) BitSet[B] {
+	return BitSet[B]{
+		set: set,
+	}
+}
+
 func (b *BitSet[B]) Set(bit B)     { b.set |= bit }
 func (b *BitSet[B]) Clear(bit B)   { b.set &^= bit }
 func (b BitSet[B]) Has(bit B) bool { return b.set&bit != 0 }
