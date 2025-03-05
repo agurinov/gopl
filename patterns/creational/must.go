3aec2343329d6c9d4620ba58e163ad8c2e4e762d
Aleksandr Gurinov <alexander.gurinov@gmail.com>
2025-03-05 10:49:59 +0700

    fix: must method

--- patterns/creational/must.go
+++ patterns/creational/must.go
@@ -2,6 +2,12 @@ package creational
 
 import "fmt"
 
+func MustInit(err error) {
+	if err != nil {
+		panic(fmt.Errorf("can't init object\n%w", err))
+	}
+}
+
 func Must[T any](t T, err error) T {
 	if err != nil {
 		panic(fmt.Errorf("can't construct %T object\n%w", t, err))
@@ -10,8 +16,10 @@ func Must[T any](t T, err error) T {
 	return t
 }
 
-func MustInit(err error) {
+func MustDuo[T1 any, T2 any](t1 T1, t2 T2, err error) (T1, T2) {
 	if err != nil {
-		panic(fmt.Errorf("can't init object\n%w", err))
+		panic(fmt.Errorf("can't construct %T and %T objects\n%w", t1, t2, err))
 	}
+
+	return t1, t2
 }
