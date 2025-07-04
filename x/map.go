42aa952901b498dc10b871ab742817865ac3bf2d
Aleksei Semushkin <lehsem80@gmail.com>
2025-07-04 17:29:56 +0200

    add MapConvert and MapFilter helpers

--- x/map.go
+++ x/map.go
@@ -36,3 +36,33 @@ func MapClone[K comparable, V any](
 
 	return out
 }
+
+func MapConvert[K1, K2 comparable, V1, V2 any](
+	in map[K1]V1,
+	mapF func(K1, V1) (K2, V2),
+) map[K2]V2 {
+	out := make(map[K2]V2, len(in))
+
+	for k, v := range in {
+		k2, v2 := mapF(k, v)
+
+		out[k2] = v2
+	}
+
+	return out
+}
+
+func MapFilter[K comparable, V any](
+	in map[K]V,
+	useF func(K, V) bool,
+) map[K]V {
+	out := make(map[K]V, len(in))
+
+	for k, v := range in {
+		if useF(k, v) {
+			out[k] = v
+		}
+	}
+
+	return out
+}
