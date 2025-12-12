73e59893cb928398bb9250db15e8dee14eb617fa
Aleksandr Gurinov <alexander.gurinov@gmail.com>
2025-12-12 11:52:56 +0400

    fix: tool for appcmd

--- appcmd/run_wait.go
+++ appcmd/run_wait.go
@@ -5,11 +5,34 @@ import (
 	"context"
 	"errors"
 
+	"github.com/agurinov/gopl/graceful"
 	"go.uber.org/zap"
 	"golang.org/x/sync/errgroup"
 )
 
+func Start(
+	ctx context.Context,
+	logger *zap.Logger,
+	stack ...graceful.Closure,
+) {
+	g, gCtx := errgroup.WithContext(ctx)
+
+	for _, f := range stack {
+		if f == nil {
+			continue
+		}
+
+		g.Go(func() error {
+			return f(gCtx)
+		})
+	}
+
+	RunWait(g, logger)
+}
+
 func RunWait(g *errgroup.Group, logger *zap.Logger) {
+	logger.Info("starting application")
+
 	waitErr := g.Wait()
 
 	isSuccess := cmp.Or(
