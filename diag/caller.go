3a55682d92c9af1b2667dfd3c7f812eac60f655a
Aleksandr Gurinov <alexander.gurinov@gmail.com>
2025-12-16 11:30:54 +0200

    fix: func name toolset

--- diag/caller.go
+++ diag/caller.go
@@ -3,16 +3,46 @@ package diag
 import (
 	"cmp"
 	"fmt"
+	"reflect"
 	"runtime"
 	"strings"
 )
 
-//nolint:gomnd,mnd
+func FunctionName(f any) string {
+	v := reflect.ValueOf(f)
+
+	isNil := cmp.Or(
+		f == nil,
+	)
+
+	isInvalid := cmp.Or(
+		!v.IsValid(),
+		v.Kind() != reflect.Func,
+	)
+
+	switch {
+	case isNil:
+		return "nil"
+	case isInvalid:
+		return "invalid"
+	}
+
+	pc := v.Pointer()
+
+	return pcName(pc)
+}
+
+//nolint:gomnd
 func CallerName(skip int) string {
 	skip = cmp.Or(skip, 1)
 
 	pc, _, _, _ := runtime.Caller(skip) //nolint:dogsled
 
+	return pcName(pc)
+}
+
+//nolint:mnd
+func pcName(pc uintptr) string {
 	fn := runtime.FuncForPC(pc)
 	if fn == nil {
 		return "unknown"
@@ -32,15 +62,15 @@ func CallerName(skip int) string {
 		return fullName
 	case n > 2:
 		var (
-			packageName = parts[n-3]
+			packageName = parts[0]
 			structName  = strings.Trim(parts[n-2], "()")
-			methodName  = parts[n-1]
+			methodName  = strings.TrimSuffix(parts[n-1], "-fm")
 		)
 
 		return fmt.Sprintf("%s.%s.%s", packageName, structName, methodName)
 	case n == 2:
 		var (
-			packageName  = parts[n-2]
+			packageName  = parts[0]
 			functionName = parts[n-1]
 		)
 
