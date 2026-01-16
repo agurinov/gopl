facb641a827bb16b2dd8eb760680d64b408361db
Aleksandr Gurinov <alexander.gurinov@gmail.com>
2026-01-16 16:34:37 +0200

    fix: new codegen interfaces

--- codegen.go
+++ codegen.go
@@ -1,5 +1,5 @@
 //go:generate -command stringer go run ./vendor/golang.org/x/tools/cmd/stringer
-//go:generate -command mockery  go run ./vendor/github.com/vektra/mockery
+//go:generate -command mockery  go run ./vendor/github.com/vektra/mockery/v3
 
 //go:generate -command protoc_gen_types    protoc      --go_out=paths=import:.
 //go:generate -command protoc_gen_grpc     protoc --go-grpc_out=paths=import,require_unimplemented_servers=false:.
--- fsm/codegen.go
+++
@@ -1,4 +0,0 @@
-//go:generate -command mockgen go run ../vendor/go.uber.org/mock/mockgen
-package fsm
-
-//go:generate mockgen -source=state_storage_iface.go -destination=gomock/state_storage_iface.go -package=gomock -mock_names=StateStorage=StateStorage
