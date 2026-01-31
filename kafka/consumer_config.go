cd354ecfd628d1eeaa8113438a05cf1669565873
Aleksandr Gurinov <alexander.gurinov@gmail.com>
2026-01-31 13:34:18 +0200

    fix: kafka consumer and producer

--- kafka/consumer_config.go
+++ kafka/consumer_config.go
@@ -3,6 +3,10 @@ package kafka
 import (
 	"time"
 
+	"github.com/twmb/franz-go/pkg/kgo"
+	"go.uber.org/zap/zapcore"
+
+	"github.com/agurinov/gopl/backoff"
 	"github.com/agurinov/gopl/backoff/strategies"
 )
 
@@ -18,3 +22,48 @@ type (
 		Idle            strategies.ExponentialConfig
 	}
 )
+
+func (c ConsumerConfig) NewConsumer(
+	opts ...ConsumerOption,
+) (
+	Consumer,
+	error,
+) {
+	backoffOptions := []backoff.Option{
+		backoff.WithName("idle"),
+		backoff.WithLogLevel(zapcore.DebugLevel),
+		backoff.WithUnlimitedRetries(),
+		backoff.WithExponentialStrategy(),
+	}
+
+	kgoConsumerOptions := []kgo.Opt{
+		kgo.SeedBrokers(c.Brokers...),
+		kgo.ConsumeTopics(c.Topic),
+		kgo.ConsumerGroup(c.Group),
+		kgo.FetchMaxWait(c.MaxPollDuration), // TODO: doesn't work
+	}
+
+	defaults := []ConsumerOption{
+		WithConsumerTopic(c.Topic),
+		WithBackoffOptions(backoffOptions...),
+		WithConsumerClientOptions(kgoConsumerOptions...),
+		WithMaxPollDuration(c.MaxPollDuration),
+		WithMaxPollRecords(c.MaxPollRecords),
+	}
+
+	if c.DLQ.Topic != "" {
+		// TODO: minimaze config in this case (merge base configs?)
+		dlq, err := c.DLQ.NewProducer()
+		if err != nil {
+			return nil, err
+		}
+
+		defaults = append(defaults,
+			WithDLQ(dlq),
+		)
+	}
+
+	opts = append(defaults, opts...)
+
+	return NewConsumer(opts...)
+}
--- kafka/consumer_metrics.go
+++ kafka/consumer_metrics.go
@@ -1,6 +1,30 @@
 //nolint:lll
 package kafka
 
+import (
+	"strconv"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+
+	"github.com/agurinov/gopl/diag/metrics"
+)
+
+type (
+	consumerMetrics struct {
+		notMyRecord *prometheus.CounterVec
+		idle        *prometheus.HistogramVec
+		discarded   *prometheus.CounterVec
+		committed   *prometheus.CounterVec
+		polling     *prometheus.HistogramVec
+		// handler work ??? via middleware
+
+		// Separate operations??
+		// client switch lock
+		// rebalance
+	}
+)
+
 const (
 	KafkaConsumerHandlerDurationHistogramName      = "gopl_kafka_consumer_handler_duration_seconds"
 	KafkaConsumerHandlerBatchDurationHistogramName = "gopl_kafka_consumer_handler_batch_duration_seconds"
@@ -11,3 +35,115 @@ const (
 	KafkaConsumerPollingDurationHistogramName      = "gopl_kafka_consumer_polling_duration_seconds"
 	KafkaConsumerCooldownDurationHistogramName     = "gopl_kafka_consumer_cooldown_duration_seconds"
 )
+
+func newConsumerMetrics() consumerMetrics {
+	return consumerMetrics{
+		notMyRecord: metrics.NewCounter(
+			KafkaConsumerNotMyRecordCounterName,
+			[]string{"topic", "partition"},
+			metrics.WithoutServicePrefix(),
+		),
+		idle: metrics.NewHistogram(
+			KafkaConsumerIdleDurationHistogramName,
+			[]string{"topic", "partition"},
+			metrics.WithoutServicePrefix(),
+		),
+		discarded: metrics.NewCounter(
+			KafkaConsumerDiscardedCounterName,
+			[]string{"topic", "partition"},
+			metrics.WithoutServicePrefix(),
+		),
+		committed: metrics.NewCounter(
+			KafkaConsumerCommittedCounterName,
+			[]string{"topic", "partition"},
+			metrics.WithoutServicePrefix(),
+		),
+		polling: metrics.NewHistogram(
+			KafkaConsumerPollingDurationHistogramName,
+			[]string{"topic", "partition"},
+			metrics.WithoutServicePrefix(),
+		),
+	}
+}
+
+func (m consumerMetrics) discardedAdd(
+	topic string,
+	partition int32,
+	cnt int,
+) {
+	if m.discarded == nil {
+		return
+	}
+
+	m.discarded.WithLabelValues(
+		topic,
+		strconv.Itoa(int(partition)),
+	).Add(
+		float64(cnt),
+	)
+}
+
+func (m consumerMetrics) committedAdd(
+	topic string,
+	partition int32,
+	cnt int,
+) {
+	if m.committed == nil {
+		return
+	}
+
+	m.committed.WithLabelValues(
+		topic,
+		strconv.Itoa(int(partition)),
+	).Add(
+		float64(cnt),
+	)
+}
+
+func (m consumerMetrics) notMyRecordInc(
+	topic string,
+	partition int32,
+) {
+	if m.notMyRecord == nil {
+		return
+	}
+
+	m.notMyRecord.WithLabelValues(
+		topic,
+		strconv.Itoa(int(partition)),
+	).Inc()
+}
+
+func (m consumerMetrics) idleObserve(
+	topic string,
+	partition int32,
+	elapsedTime time.Duration,
+) {
+	if m.idle == nil {
+		return
+	}
+
+	m.idle.WithLabelValues(
+		topic,
+		strconv.Itoa(int(partition)),
+	).Observe(
+		elapsedTime.Seconds(),
+	)
+}
+
+func (m consumerMetrics) pollingObserve(
+	topic string,
+	partition int32,
+	elapsedTime time.Duration,
+) {
+	if m.polling == nil {
+		return
+	}
+
+	m.polling.WithLabelValues(
+		topic,
+		strconv.Itoa(int(partition)),
+	).Observe(
+		elapsedTime.Seconds(),
+	)
+}
