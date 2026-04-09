dec07cde700466eb95f9195e9c2dda34ed9f693e
Aleksandr Gurinov <alexander.gurinov@gmail.com>
2026-04-09 18:04:17 +0300

    fix: loger fully oneliner and config options pattern

--- diag/log/config.go
+++ diag/log/config.go
@@ -10,8 +10,9 @@ type (
 	Config struct {
 		Format          string `validate:"oneof=json console"`
 		Level           string `validate:"oneof=debug info warn error"`
-		EnableCaller    bool   `json:"enable_caller" yaml:"enable_caller"`
-		EnableTraceback bool   `json:"enable_traceback" yaml:"enable_traceback"`
+		zapFields       []zap.Field
+		EnableCaller    bool `json:"enable_caller" yaml:"enable_caller"`
+		EnableTraceback bool `json:"enable_traceback" yaml:"enable_traceback"`
 	}
 	Option = c.Option[Config]
 )
@@ -22,6 +23,7 @@ func (cfg Config) New(opts ...Option) (*zap.Logger, *zap.AtomicLevel, error) {
 		WithLevel(cfg.Level),
 		WithCaller(cfg.EnableCaller),
 		WithTraceback(cfg.EnableTraceback),
+		WithFields(cfg.zapFields...),
 	}
 
 	opts = append(defaults, opts...)
